291c04fb629ac4850ce58b33ac5dc1a08c7a6ec7
Jurian Baas <jurian.baas@gmail.com>
2025-04-18 12:44:02 +0200

    add header and content wrapper (WIP)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,25 @@
 <script>
-    import '../main.css'
+	import '../main.css';
+	import Header from '$lib/components/Header.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="ams-screen ams-screen--wide ams-docs-screen">
+	<div class="ams-grid">
+		<div class="ams-grid__cell ams-grid__cell--span-all">
+			<Header appTitle="BiasBuster" />
+		</div>
+		<div class="ams-grid__cell ams-grid__cell--span-all">
+			<div class="wrapper">
+				{@render children()}
+			</div>
+		</div>
+	</div>
+</div>
 
 <style>
-	
+	.wrapper {
+		padding-inline: var(--ams-header-padding-inline);
+		/* TODO: haha we use the same padding var as used in the header to pad the app wrapper, NOT the way */
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+	<p class="ams-paragraph ams-paragraph--large ams-mb-m">
+		Niet meer naar het Stadsloket, maar thuis of op het werk uw nieuwe paspoort ontvangen. In juni
+		en juli kan dat. Deze zomer bezorgt de gemeente reisdocumenten gratis aan huis. We doen dat om
+		de drukte in de Stadsloketten te verminderen. En u hoeft maar 1 keer naar het Stadsloket.
+	</p>
+
+
