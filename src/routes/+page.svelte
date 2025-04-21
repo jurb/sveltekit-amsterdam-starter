0a04771d0306a1e8ccf1af8083016b5b20306243
Jurian Baas <jurian.baas@gmail.com>
2025-04-21 10:12:41 +0200

    simplify paragraph styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 
-	<p class="ams-paragraph ams-paragraph--large ams-mb-m">
+	
+	<p class="ams-paragraph--large">
+		Niet meer naar het Stadsloket, maar thuis of op het werk uw nieuwe paspoort ontvangen. In juni
+		en juli kan dat. Deze zomer bezorgt de gemeente reisdocumenten gratis aan huis. We doen dat om
+		de drukte in de Stadsloketten te verminderen. En u hoeft maar 1 keer naar het Stadsloket.
+	</p>
+	<p class="">
 		Niet meer naar het Stadsloket, maar thuis of op het werk uw nieuwe paspoort ontvangen. In juni
 		en juli kan dat. Deze zomer bezorgt de gemeente reisdocumenten gratis aan huis. We doen dat om
 		de drukte in de Stadsloketten te verminderen. En u hoeft maar 1 keer naar het Stadsloket.
