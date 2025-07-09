7ae6f1ddacd670b8e2fe484eb8dca890145cb97a
Jurian Baas <jurian.baas@gmail.com>
2025-07-09 10:49:27 +0200

    extra wrapper for bg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,23 @@
 
 <Header appTitle="Test app" />
 
-<div class="grid-wrapper ams-screen ams-screen--wide">
-	<div class="ams-grid">
-		<div class="ams-grid__cell ams-grid__cell--span-all">
-			{@render children()}
+<div class="grid-wrapper-wrapper">
+	<div class="grid-wrapper ams-screen ams-screen--wide">
+		<div class="ams-grid">
+			<div class="ams-grid__cell ams-grid__cell--span-all">
+				{@render children()}
+			</div>
 		</div>
 	</div>
 </div>
 
 <style>
-	.grid-wrapper {
+	.grid-wrapper-wrapper {
 		background-color: #f5f5f5;
 		min-height: 100vh; /* Make it at least full viewport height */
-		width: 100%;
+		width: 100vw;
+	}
+	.grid-wrapper {
+		background-color: #f5f5f5;
 	}
 </style>
